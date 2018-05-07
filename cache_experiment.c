1f9d8ac3b7847b390d9b6b35cd903f4d51da0d5d
Hendrik Makait <hendrik.makait@gmail.com>
2018-05-07 13:06:36 +0200

    Refactor and add baseline

--- cache_experiment.c
+++ cache_experiment.c
@@ -4,12 +4,16 @@
 
 const int KB = 1024;
 const int MB = 1024 * 1024;
-const int ITERATIONS = 1000000000; // one billion iterations
+const int ITERATIONS = 1000 * 1000 * 1000; // one billion iterations
 const int INT_SIZE = sizeof(int);
 
 int *init_looping_indices(int array_size, int stride);
 
-int main() {
+void establish_base_line(FILE *fp);
+
+void probe_access_latency(int array_size, int stride, FILE *fp);
+
+void run_experiment() {
     int array_sizes[] = {4 * KB, 8 * KB, 16 * KB,
                          24 * KB, 32 * KB, 48 * KB, // narrow band around 32 KB
                          64 * KB, 128 * KB,
@@ -21,38 +25,56 @@ int main() {
                      48, 64, 96, // narrow band around 64 B
                      128, 256, 512, 1 * KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB};
     int array_size, stride;
-    double average_access_time;
 
     FILE *fp;
-    fp = fopen("access_times.tsv", "w");
+    fp = fopen("access_times_baseline.tsv", "w");
 
     int array_sizes_length = sizeof(array_sizes) / INT_SIZE;
     fprintf(fp, "Stride\tArray Size\tAccess Time\n");
+    establish_base_line(fp);
     for (int stride_index = 0; stride_index < sizeof(strides) / INT_SIZE; stride_index++) {
         stride = strides[stride_index];
         for (int array_size_index = 0; array_size_index < array_sizes_length; array_size_index++) {
             array_size = array_sizes[array_size_index];
-            /* init array and fill with offsets */
-            struct timespec start, end;
-            int *looping_indices = init_looping_indices(array_size, stride);
-            int looping_index = 0;
-            clock_gettime(CLOCK_REALTIME, &start);
-#pragma unroll(100)
-            for (int _ = 0; _ < ITERATIONS; _++) {
-                //looping_index = _;
-                looping_index = looping_indices[looping_index];
-            }
-            clock_gettime(CLOCK_REALTIME, &end);
-            double access_time = (end.tv_sec - start.tv_sec) * 1000000000L; // convert seconds to nanoseconds
-            access_time += (end.tv_nsec - start.tv_nsec);
-            average_access_time = access_time / ITERATIONS;
-            printf("Stride: %8d\tArray Size: %12d\tAverage Access Time: %8.5f ns\n", stride, array_size,
-                   average_access_time);
-            fprintf(fp, "%d\t%d\t%f\n", stride, array_size, average_access_time);
-            free(looping_indices);
+            probe_access_latency(array_size, stride, fp);
         }
     }
     fclose(fp);
+}
+void establish_base_line(FILE *fp) {
+    struct timespec start, end;
+    int looping_index;
+    clock_gettime(CLOCK_REALTIME, &start);
+#pragma unroll(100) // loop unrolling
+    for (int _ = 0; _ < ITERATIONS; _++) {
+        looping_index = _;
+    }
+    clock_gettime(CLOCK_REALTIME, &end);
+    double access_time = (end.tv_sec - start.tv_sec) * 1000000000L; // convert seconds to nanoseconds
+    access_time += (end.tv_nsec - start.tv_nsec);
+    double average_access_time = access_time / ITERATIONS;
+    printf("Stride: None\tArray Size: None\tAverage Access Time: %8.5f ns\n", average_access_time);
+    fprintf(fp, "None\tNone\t%f\n", average_access_time);
+}
+
+void probe_access_latency(int array_size, int stride, FILE *fp) {
+/* init array and fill with offsets */
+    struct timespec start, end;
+    int *looping_indices = init_looping_indices(array_size, stride);
+    int looping_index = 0;
+    clock_gettime(CLOCK_REALTIME, &start);
+#pragma unroll(100) // loop unrolling
+    for (int _ = 0; _ < ITERATIONS; _++) {
+        looping_index = looping_indices[looping_index];
+    }
+    clock_gettime(CLOCK_REALTIME, &end);
+    double access_time = (end.tv_sec - start.tv_sec) * 1000000000L; // convert seconds to nanoseconds
+    access_time += (end.tv_nsec - start.tv_nsec);
+    double average_access_time = access_time / ITERATIONS;
+    printf("Stride: %8d\tArray Size: %12d\tAverage Access Time: %8.5f ns\n", stride, array_size,
+           average_access_time);
+    fprintf(fp, "%d\t%d\t%f\n", stride, array_size, average_access_time);
+    free(looping_indices);
 }
 
 int *init_looping_indices(int array_size, int stride) {
@@ -70,4 +92,14 @@ int *init_looping_indices(int array_size, int stride) {
         looping_indices[step] = next_step;
     }
     return looping_indices;
+}
+
+int main() {
+    if (sizeof(int) != 4) { // Make sure 4 byte int is used
+        perror("Integer size must be 4 bytes to fit currently used numbers.");
+        exit(EXIT_FAILURE);
+    } else {
+        run_experiment();
+    }
+    exit(EXIT_SUCCESS);
 }
